557ac34e6142d285b93540959408c1eb99b00810
Imani Paul <imanipaul@imanis-mbp.lan>
2020-04-06 18:21:45 -0400

    Basic functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [skiData, setSkiData] = useState({
+    total: 50,
+    powder: 20,
+    backcountry: 10,
+    goal: 100,
+  });
+
+  const [goalProgress, setGoalProgress] = useState(0);
+
+  const getPercent = (decimal) => {
+    return decimal * 100 + "%";
+  };
+
+  const calcGoalProgress = (total, goal) => {
+    return getPercent(total / goal);
+  };
+
+  useEffect(() => {
+    setGoalProgress(calcGoalProgress(skiData.total, skiData.goal));
+  }, [skiData]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <h3>Total Days: {skiData.total}</h3>
+        <h3>Powder Days: {skiData.powder}</h3>
+        <h3>Backcountry Days: {skiData.backcountry}</h3>
+        <h3>Goal Progress: {goalProgress}</h3>
+      </section>
+
+      <section>
+        <h4>Update values</h4>
+      </section>
     </div>
   );
 }
