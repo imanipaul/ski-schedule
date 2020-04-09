9f7653936ffa4743169d2fd5c9826bed346e5cfa
IPaul <ipaul@NY-COU-IPAUL.turner.com>
2020-04-09 12:50:31 -0400

    Remove build folder from git ignore

---
+++ build/precache-manifest.b69e8f001338917f8796886abb5fee69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333c4b293af01425c59464f0a4ef0183",
+    "url": "/ski-schedule/index.html"
+  },
+  {
+    "revision": "6deb76b4d4381e926dc2",
+    "url": "/ski-schedule/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "401d0e20b9934202bc2c",
+    "url": "/ski-schedule/static/css/main.4bccf723.chunk.css"
+  },
+  {
+    "revision": "6deb76b4d4381e926dc2",
+    "url": "/ski-schedule/static/js/2.26badd38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ski-schedule/static/js/2.26badd38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401d0e20b9934202bc2c",
+    "url": "/ski-schedule/static/js/main.8589367a.chunk.js"
+  },
+  {
+    "revision": "5e8bd02fbf69e14dca0b",
+    "url": "/ski-schedule/static/js/runtime-main.56cde502.js"
+  }
+]);
