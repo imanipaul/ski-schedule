7ccad7efdacc2a981bbc0f6cb6a718143420ac0d
Imani Paul <imanipaul@imanis-mbp.lan>
2020-04-08 22:30:07 -0400

    Update calendar styles

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,65 +1,55 @@
-import React, {useState} from 'react';
-import DayPicker from 'react-day-picker';
-import 'react-day-picker/lib/style.css';
+import React, { useState } from "react";
+import DayPicker from "react-day-picker";
+import "react-day-picker/lib/style.css";
 
 export default function Example() {
-    const [selectedDays, setSelectedDays] = useState([])
+  const [selectedDays, setSelectedDays] = useState([]);
   const modifiers = {
     thursdays: { daysOfWeek: [4] },
     birthday: new Date(2020, 3, 2),
   };
   const modifiersStyles = {
     birthday: {
-      color: 'white',
-      backgroundColor: 'red',
+      color: "white",
+      backgroundColor: "red",
     },
     thursdays: {
-      color: '#ffc107',
-      backgroundColor: '#fffdee',
+      color: "#ffc107",
+      backgroundColor: "#fffdee",
     },
     // outside: {
     //   // backgroundColor: 'white',
     // },
     selected: {
-        backgroundColor: 'purple',
-    }
+      backgroundColor: "#becbf4",
+      color: "black",
+    },
+    today: {
+      color: "#fed801",
+    },
+    outside: {
+      color: "white",
+    },
+    navButtonPrev: {
+      color: "#fed801",
+    },
   };
 
-  const handleDayClick = (day, {selected}) => {
-
-    setSelectedDays([...selectedDays, selected ? undefined : day])
-   
-  }
+  const handleDayClick = (day, { selected }) => {
+    setSelectedDays([...selectedDays, selected ? undefined : day]);
+  };
 
   return (
-    <section className = 'calendar'>
-
+    <section className="calendar">
+      <h3>Calendar</h3>
       <DayPicker
-      //   month={new Date(2020, 3)}
-      //   modifiers={modifiers}
+        //   month={new Date(2020, 3)}
+        //   modifiers={modifiers}
         modifiersStyles={modifiersStyles}
-      selectedDays={selectedDays}
-      onDayClick={handleDayClick}
-      showOutsideDays
-  
+        selectedDays={selectedDays}
+        onDayClick={handleDayClick}
+        showOutsideDays
       />
     </section>
   );
 }
-
-// render() {
-//     return (
-//       <div>
-//         <DayPicker
-//           selectedDays={this.state.selectedDay}
-//           onDayClick={this.handleDayClick}
-//         />
-//         <p>
-//           {this.state.selectedDay
-//             ? this.state.selectedDay.toLocaleDateString()
-//             : 'Please select a day 👻'}
-//         </p>
-//       </div>
-//     );
-//   }
-// }
